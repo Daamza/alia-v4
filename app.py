ca346c663a365813d714191e8487dc741469faec
Daamza <113866058+Daamza@users.noreply.github.com>
2025-04-24 19:36:02 -0300

    Update app.py

--- app.py
+++ app.py
@@ -91,7 +91,7 @@ def whatsapp_webhook():
         media_url = request.form.get("MediaUrl0")
         image_response = requests.get(media_url, auth=(os.getenv("TWILIO_ACCOUNT_SID"), os.getenv("TWILIO_AUTH_TOKEN")))
         image_base64 = base64.b64encode(image_response.content).decode()
-        ocr_response = requests.post("https://ocr-microsistema.onrender.com", json={"image_base64": image_base64})
+        ocr_response = requests.post("https://ocr-microsistema.onrender.com/ocr", json={"image_base64": image_base64})
 
         if ocr_response.ok:
             try:
